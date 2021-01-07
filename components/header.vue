95544dcb95fc7d7b1f58266f858bab07ca017be0
Maksim-py-js <muhamedzyanov2004@gmail.com>
2021-01-07 11:27:08 +0500

    Add page map

--- components/header.vue
+++ components/header.vue
@@ -9,6 +9,7 @@
                     <b-navbar-nav class="text-center h-100">
                         <nuxt-link to="/" class="mr-3 p-2 text-white text-decoration-none">О компании</nuxt-link>
                         <nuxt-link to="/object_page" class="mr-3 p-2 text-white text-decoration-none">Объекты</nuxt-link>
+                        <nuxt-link to="/map" class="mr-3 p-2 text-white text-decoration-none">Карта</nuxt-link>
                         <nuxt-link to="/" class="mr-3 p-2 text-white text-decoration-none">Уникальности проектов</nuxt-link>
                         <nuxt-link to="/" class="mr-3 p-2 text-white text-decoration-none">Карьера</nuxt-link>
                         <nuxt-link to="/" class="mr-3 p-2 text-white text-decoration-none">Тендеры</nuxt-link>
@@ -136,78 +137,7 @@
             <b-button class="mr-3" variant="primary">Поиск на карте</b-button>
             </div>
         </b-container>
-        <b-container class="position-relative">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d95913.44056607499!2d69.23376702582306!3d41.30244525704403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1609333243458!5m2!1sru!2s" width="1140" height="707" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-            <div class="position-absolute h-100 bg-white p-3" style="top: 0; right: 0; width: 310px;">
-                <div class="mb-2">
-                    <b-button variant="light" class="mr-1 mb-1">Москва и МО</b-button>
-                    <b-button variant="light" class="mr-1 mb-1">Москва</b-button>
-                    <b-button variant="light" class="mr-1 mb-1">Подмосковье</b-button>
-                    <b-button variant="light" class="mr-1 mb-1">Новая Москва</b-button>
-                    <b-button variant="light" class="mr-1 mb-1">Санкт Петербург</b-button>
-                </div>
-                <div>
-                    <div class="text-secondary"><small>Цена</small></div>
-                    <div class="d-flex mt-2">          
-                        <b-form class="d-flex">
-                            <b-form-group class="mr-1">
-                            <b-form-input
-                                id="input-1"
-                                type="number"
-                                placeholder="от 0 млн"
-                                required
-                            ></b-form-input>
-                            </b-form-group>
-                            <b-form-group>
-                            <b-form-input
-                                id="input-1"
-                                type="number"
-                                placeholder="до 850 млн"
-                                required
-                            ></b-form-input>
-                            </b-form-group>
-                        </b-form>
-                    </div>
-                </div>
-                <div>
-                    <label for="range-2" class="text-secondary"><small>Срок сдачи</small></label>
-                    <b-form-input id="range-2" v-model="deadlineRangeValue" type="range" min="2020" max="2024" step="1"></b-form-input>
-                    <div class="mt-2">{{ deadlineRangeValue }}</div>
-                </div>
-                <div class="mt-3">
-                    <div class="text-secondary mb-2"><small>Студия</small></div>
-                    <b-form-select v-model="selected_roominessOptions" :options="roominessOptions"></b-form-select>
-                </div>
-                <div>
-                    <div class="text-secondary"><small>Площадь</small></div>
-                    <div class="d-flex mt-2">          
-                        <b-form class="d-flex">
-                            <b-form-group class="mr-1">
-                            <b-form-input
-                                id="input-1"
-                                type="number"
-                                placeholder="от 20 м²"
-                                required
-                            ></b-form-input>
-                            </b-form-group>
-                            <b-form-group>
-                            <b-form-input
-                                id="input-1"
-                                type="number"
-                                placeholder="до 200 м²"
-                                required
-                            ></b-form-input>
-                            </b-form-group>
-                        </b-form>
-                    </div>
-                </div>
-                <div class="mb-2">
-                    <b-button variant="light" class="mr-1 mb-1">Квартиры</b-button>
-                    <b-button variant="light" class="mr-1 mb-1">Апартаменты</b-button>
-                    <b-button variant="light" class="mr-1 mb-1">Танхаусы</b-button>
-                </div>
-            </div>
-        </b-container>
+        
     </div>
 </template>
 
