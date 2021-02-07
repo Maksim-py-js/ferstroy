b6671fd642905d185d5972650069bc40dc3730e8
PaVeL-58 <kruglyakovskiypavel123@mail.ru>
2021-02-07 22:01:27 +0500

    empty changes

--- pages/map.vue
+++ pages/map.vue
@@ -14,6 +14,7 @@
                         :icon="markerIcon"
                         :balloon-template="balloonTemplate"
                         :options="balloonOptions"
+                        class="mapMarker"
                     />
                     <ymap-marker 
                         marker-id="124" 
@@ -21,7 +22,13 @@
                         :icon="markerIcon"      
                         :balloon-template="balloonTemplate"
                         :options="balloonOptions"
+                        class="mapMarker"
                     />
+                    <script>
+                        let markerPath = document.querySelectorAll('ymaps.ymaps-2-1-78-inner-panes');
+                        let markerBox = markerPath[0].children;
+                        console.log(markerBox.item(4));
+                    </script>
                 </yandex-map>
             </no-ssr>
             <div class="position-absolute h-99 bg-white filterBox">
@@ -120,7 +127,35 @@
                         пользуются большим спросом, на нашем сайте можете размещать не ограниченное колличество квартир в любой из категорий. А так же размещать рекламные и информационные статьи. Удачи!
                     </span>
                     <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d95913.44056607499!2d69.23376702582306!3d41.30244525704403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1609333243458!5m2!1sru!2s" width="100%" height="400px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        <no-ssr>
+                            <yandex-map 
+                                :coords="coords" 
+                                :zoom="14" 
+                                style="height: 400px"
+                            >
+                                <ymap-marker 
+                                    marker-id="123" 
+                                    :coords="markers[0]"
+                                    :icon="markerIcon"
+                                    :balloon-template="balloonTemplate"
+                                    :options="balloonOptions"
+                                    class="mapMarker"
+                                />
+                                <ymap-marker 
+                                    marker-id="124" 
+                                    :coords="markers[1]"
+                                    :icon="markerIcon"      
+                                    :balloon-template="balloonTemplate"
+                                    :options="balloonOptions"
+                                    class="mapMarker"
+                                />
+                                <script>
+                                    let markerPath = document.querySelectorAll('ymaps.ymaps-2-1-78-inner-panes');
+                                    let markerBox = markerPath[0].children;
+                                    console.log(markerBox.item(4));
+                                </script>
+                            </yandex-map>
+                        </no-ssr>
                         <div class="position-absolute h-100 bg-white p-3 miniFilter">
                             <div class="mb-2">
                                 <div class="cityBtn">Фергана и Обл</div>
@@ -174,7 +209,35 @@
                         Квартиры в новостройках Ферганы пользуются большим спросом, на нашем сайте можете размещать не ограниченное колличество квартир в любой из категорий. А так же размещать рекламные и информационные статьи. Удачи!
                     </span>
                     <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d95913.44056607499!2d69.23376702582306!3d41.30244525704403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1609333243458!5m2!1sru!2s" width="100%" height="400px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        <no-ssr>
+                            <yandex-map 
+                                :coords="coords" 
+                                :zoom="14" 
+                                style="height: 400px"
+                            >
+                                <ymap-marker 
+                                    marker-id="123" 
+                                    :coords="markers[0]"
+                                    :icon="markerIcon"
+                                    :balloon-template="balloonTemplate"
+                                    :options="balloonOptions"
+                                    class="mapMarker"
+                                />
+                                <ymap-marker 
+                                    marker-id="124" 
+                                    :coords="markers[1]"
+                                    :icon="markerIcon"      
+                                    :balloon-template="balloonTemplate"
+                                    :options="balloonOptions"
+                                    class="mapMarker"
+                                />
+                                <script>
+                                    let markerPath = document.querySelectorAll('ymaps.ymaps-2-1-78-inner-panes');
+                                    let markerBox = markerPath[0].children;
+                                    console.log(markerBox.item(4));
+                                </script>
+                            </yandex-map>
+                        </no-ssr>
                         <div class="position-absolute h-100 bg-white p-3 miniFilter">
                             <div class="mb-2">
                                 <div class="cityBtn">Фергана и Обл</div>
@@ -237,7 +300,35 @@
                         Квартиры в новостройках Ферганы пользуются большим спросом, на нашем сайте можете размещать не ограниченное колличество квартир в любой из категорий. А так же размещать рекламные и информационные статьи. Удачи!
                     </span>
                     <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d95913.44056607499!2d69.23376702582306!3d41.30244525704403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2s!4v1609333243458!5m2!1sru!2s" width="100%" height="400px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        <no-ssr>
+                            <yandex-map 
+                                :coords="coords" 
+                                :zoom="14" 
+                                style="height: 400px"
+                            >
+                                <ymap-marker 
+                                    marker-id="123" 
+                                    :coords="markers[0]"
+                                    :icon="markerIcon"
+                                    :balloon-template="balloonTemplate"
+                                    :options="balloonOptions"
+                                    class="mapMarker"
+                                />
+                                <ymap-marker 
+                                    marker-id="124" 
+                                    :coords="markers[1]"
+                                    :icon="markerIcon"      
+                                    :balloon-template="balloonTemplate"
+                                    :options="balloonOptions"
+                                    class="mapMarker"
+                                />
+                                <script>
+                                    let markerPath = document.querySelectorAll('ymaps.ymaps-2-1-78-inner-panes');
+                                    let markerBox = markerPath[0].children;
+                                    console.log(markerBox.item(4));
+                                </script>
+                            </yandex-map>
+                        </no-ssr>
                         <div class="position-absolute h-100 bg-white p-3 miniFilter">
                             <div class="mb-2">
                                 <div class="cityBtn">Фергана и Обл</div>
@@ -386,7 +477,6 @@
                     [40.381318, 71.804794],
                     [40.377401, 71.786434]          
                 ],
-                markColor: '#77C85B',
                 markerIcon: {
                     layout: 'default#imageWithContent',
                     imageHref: ``,
@@ -394,21 +484,8 @@
                     imageOffset: [-40, -110],
                     content: '',
                     contentOffset: [0, 0],
-                    contentLayout: `<div class="marksItem">
-                        <svg width="89" height="114" viewBox="0 0 89 114" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M66.1622 67.2483C50.6719 59.392 34.1273 60.7714 29.2829 70.3232C27.3658 74.1032 27.4804 78.6769 29.6201 83.5506L41.6343 111.317C41.665 111.385 41.7361 111.441 41.7748 111.508C42.4647 112.719 44.049 113.306 45.0962 113.073C53.5679 111.187 69.1827 107.558 75.1525 106.23C75.1525 106.23 75.1553 106.228 75.1576 106.227L75.1944 106.219C80.08 105 83.6326 102.442 85.4696 98.8198C90.314 89.2681 81.6524 75.1046 66.1622 67.2483Z" fill="url(#paint0_linear)"/>
-                            <path d="M44.5 0C19.9634 0 0 19.3071 0 43.0371C0 52.428 3.06708 61.3511 8.87718 68.8373L41.757 111.665C41.8391 111.769 41.9646 111.808 42.0559 111.901C43.699 113.603 46.0701 113.175 47.2407 111.665C56.709 99.4468 74.064 76.6281 80.7367 68.0207C80.7367 68.0207 80.739 68.0141 80.7413 68.0097L80.7823 67.9567C86.1588 60.6514 89 52.0352 89 43.0371C89 19.3071 69.0366 0 44.5 0ZM44.5 66.2551C31.2892 66.2551 20.4928 55.8136 20.4928 43.0371C20.4928 30.2606 31.2892 19.8191 44.5 19.8191C57.7108 19.8191 68.5072 30.2606 68.5072 43.0371C68.5072 55.8136 57.7108 66.2551 44.5 66.2551Z" class="MarkersSvg"/>
-                            <defs>
-                                <linearGradient id="paint0_linear" x1="66.1622" y1="67.2483" x2="43.0934" y2="112.733" gradientUnits="userSpaceOnUse">
-                                    <stop offset="0.34375" stop-opacity="0"/>
-                                    <stop offset="1" stop-opacity="0.28"/>
-                                </linearGradient>
-                            </defs>
-                        </svg>
-                        <div class="markImage">
-                            <img src="https://mirabad.uz/upload/kelnik.adminpage/c0c/c0cf81c2a22ec8ec0c77335fe14b9533.jpg">
-                        </div>
-                    </div>`
+                    markerColor: '#77C85B',
+                    contentLayout: ''
                 },
                 balloonOptions: {
                     hideIconOnBalloonOpen: false,
@@ -417,7 +494,33 @@
                 showMap: false,
             }
         },
-        mounted() { this.showMap = true },
+        mounted() { 
+            this.showMap = true,
+            this.markerTemplate()
+        },
+        methods: {
+            markerTemplate() {
+                this.markerIcon.contentLayout = `<div class="marksItem">
+                    <svg width="89" height="114" viewBox="0 0 89 114" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M66.1622 67.2483C50.6719 59.392 34.1273 60.7714 29.2829 70.3232C27.3658 74.1032 27.4804 78.6769 29.6201 83.5506L41.6343 111.317C41.665 111.385 41.7361 111.441 41.7748 111.508C42.4647 112.719 44.049 113.306 45.0962 113.073C53.5679 111.187 69.1827 107.558 75.1525 106.23C75.1525 106.23 75.1553 106.228 75.1576 106.227L75.1944 106.219C80.08 105 83.6326 102.442 85.4696 98.8198C90.314 89.2681 81.6524 75.1046 66.1622 67.2483Z" fill="url(#paint0_linear)"/>
+                        <path d="M44.5 0C19.9634 0 0 19.3071 0 43.0371C0 52.428 3.06708 61.3511 8.87718 68.8373L41.757 111.665C41.8391 111.769 41.9646 111.808 42.0559 111.901C43.699 113.603 46.0701 113.175 47.2407 111.665C56.709 99.4468 74.064 76.6281 80.7367 68.0207C80.7367 68.0207 80.739 68.0141 80.7413 68.0097L80.7823 67.9567C86.1588 60.6514 89 52.0352 89 43.0371C89 19.3071 69.0366 0 44.5 0ZM44.5 66.2551C31.2892 66.2551 20.4928 55.8136 20.4928 43.0371C20.4928 30.2606 31.2892 19.8191 44.5 19.8191C57.7108 19.8191 68.5072 30.2606 68.5072 43.0371C68.5072 55.8136 57.7108 66.2551 44.5 66.2551Z" fill="${this.markerIcon.markerColor}"/>
+                        <defs>
+                            <linearGradient id="paint0_linear" x1="66.1622" y1="67.2483" x2="43.0934" y2="112.733" gradientUnits="userSpaceOnUse">
+                                <stop offset="0.34375" stop-opacity="0"/>
+                                <stop offset="1" stop-opacity="0.28"/>
+                            </linearGradient>
+                        </defs>
+                    </svg>
+                    <div class="markImage">
+                        <img src="https://mirabad.uz/upload/kelnik.adminpage/c0c/c0cf81c2a22ec8ec0c77335fe14b9533.jpg">
+                    </div>
+                </div>`;
+            }
+            // markerEnter() {
+            //     this.markerIcon.markerColor = '#FF9800';
+            //     console.log(this.markerIcon.markerColor);
+            // }
+        },
         computed: { 
             balloonTemplate() {
                 return `
