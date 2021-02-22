1bf07d33c7a736bd56bfecc0598ac01a034e91e3
PaVeL-58 <kruglyakovskiypavel123@mail.ru>
2021-02-22 17:21:42 +0500

    parlament adaptive

--- pages/parlament.vue
+++ pages/parlament.vue
@@ -1,38 +1,80 @@
 <template>
-    <div class="h-panel p-4">
+    <div class="h-panel">
         <div class="background-filter"></div>  
         <b-navbar class="panel-top p-0 mb-4">
             <!-- Right aligned nav items -->
-            <b-navbar-nav class="d-flex align-items-center justify-content-between w-100">
+            <b-navbar-nav class="align-items-center justify-content-between w-100 full-bar">
                 <!-- <b-breadcrumb variant="transparent" :items="bredcrumbsItems" class="custom-breadcrumbs mb-0 bg-transparent text-light"></b-breadcrumb> -->
-                <nuxt-link to="/" class="header__logo">
+                <nuxt-link to="/" class="header__logo admin-logo">
                     <img src="@/assets/images/svg/logo/russian-light.svg">
                 </nuxt-link>
-                <div class="position-relative header__language">
+                <div class="tabs p-0">
+                    <b-nav>                
+                        <b-nav-item
+                            v-for="item in tabsItem" 
+                            :key="item.id" 
+                            :to="item.path" 
+                            class="tabs-item pl-0 pt-2 pb-2" 
+                            :class="{active:item.active}"
+                        >   
+                            {{item.name}}
+                        </b-nav-item>
+                    </b-nav>
+                </div>
+                <div class="position-relative header__language admin-lenguage">
                     <div class="header__language_block position-absolute">
                         <b-button variant="transparent" class="header__language_btn shadow-none">Рус</b-button>
                         <b-button variant="transparent" class="header__language_btn shadow-none">Узб</b-button>
                     </div>
                 </div>
             </b-navbar-nav>
+
+            <b-navbar-nav class="align-items-center justify-content-between w-100 min-bar">
+                <div class="d-flex align-items-center">
+                    <div v-b-toggle.sidebar-1 style="outline: none;"><b-icon-list class="close-menu-icon"></b-icon-list></div>
+                    <div class="position-relative header__language admin-lenguage">
+                        <div class="header__language_block position-absolute">
+                            <b-button variant="transparent" class="header__language_btn shadow-none">Рус</b-button>
+                            <b-button variant="transparent" class="header__language_btn shadow-none">Узб</b-button>
+                        </div>
+                    </div>
+                </div> 
+                <b-sidebar 
+                    id="sidebar-1" 
+                    no-slide="true"
+                    shadow
+                    body-class="sidebar-body"
+                    header-class="sidebar-body"
+                    class="sidebar-bg"
+                    text-variant="light"
+                >
+                    <div class="px-3 py-2">
+                        <div class="tabs p-0">
+                            <b-nav>                
+                                <b-nav-item
+                                    v-for="item in tabsItem" 
+                                    :key="item.id" 
+                                    :to="item.path" 
+                                    class="tabs-item pl-0 pt-2 pb-2" 
+                                    :class="{active:item.active}"
+                                >   
+                                    {{item.name}}
+                                </b-nav-item>
+                            </b-nav>
+                        </div>
+                    </div>
+                </b-sidebar>
+                <!-- <b-breadcrumb variant="transparent" :items="bredcrumbsItems" class="custom-breadcrumbs mb-0 bg-transparent text-light"></b-breadcrumb> -->
+                <nuxt-link to="/" class="header__logo admin-logo">
+                    <img src="@/assets/images/svg/logo/russian-light.svg">
+                </nuxt-link>
+                
+            </b-navbar-nav>
         </b-navbar>
-        <b-row class="m-0">
-            <b-col cols="2" class="tabs p-0">
-                <b-nav vertical v-for="item in tabsItem" :key="item.id">                
-                    <b-nav-item 
-                        :to="item.path" 
-                        class="tabs-item pl-0 pt-2 pb-2" 
-                        :class="{active:item.active}"
-                    >   
-                        {{item.name}}
-                    </b-nav-item>
-                </b-nav>
-            </b-col>
-            <b-col class="bg-light w-100 p-0">                
-                <nuxt-child />
-                <notifications group="admin-notification"/>
-            </b-col>
-        </b-row>
+        <b-col class="bg-light w-100 p-0">                
+            <nuxt-child />
+            <notifications group="admin-notification"/>
+        </b-col>
     </div>
 </template>
 
@@ -78,10 +120,17 @@
 </script>
 
 <style>
+    .close-menu-icon {
+        color: #fff;
+        font-weight: bold;
+        font-size: 30px;
+    }
     .h-panel {
         background: url('../assets/images/png/slide_1-min.png') no-repeat 50% 50%/cover;
         position: relative;
         transition: 0.5s;
+        padding: 1.5rem;
+        min-height: 100vh;
     }
     .background-filter {
         position: absolute;
@@ -96,13 +145,17 @@
         height: 56px;
         transition: 0.5s;
     }
-    .tabs {
-        max-width: 220px;
-        width: 100%;
-        height: 100vh;
-        transition: 0.5s;
+    .full-bar {
+        display: flex;
     }
-    .tabs-item a{
+    .tabs .nav {
+        display: flex;
+        align-items: center;
+    }
+    .tabs-item {
+        margin: 0 30px;
+    }
+    .navbar .tabs .nav .tabs-item a{
         color: #fff;
         font-family: "Montserrat";
         font-size: 18px;
@@ -113,5 +166,36 @@
     .tabs-item a:hover, .tabs-item.active a {
         color: #FF9800;
         transition: 0.5s;
+    }
+    .min-bar {
+        display: none;
+    }
+    .sidebar-body {
+        background: rgba(0, 0, 0, 0.9);
+    }
+    ul div .b-sidebar {
+        background: transparent !important;
+    }
+    @media only screen and (max-width: 900px) {
+        .h-panel {
+            padding: 10px;
+        }
+    }
+    @media only screen and (max-width: 820px) {
+        .full-bar {
+            display: none;
+        }
+        .min-bar {
+            display: flex;
+        }
+        .tabs-item {
+            margin: 0;
+        }
+        .tabs .nav {
+            display: block;
+        }
+        .admin-lenguage {
+            margin: -3px 15px 0;
+        }
     }
 </style>
